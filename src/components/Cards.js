b5c4116dfdd9b34edeca46ac04577d4a0c7075ed
unknown <00090718@uca.edu.sv>
2021-06-20 03:15:02 -0600

    Cards: fixing

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,24 +2,36 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {CardDeck,Card} from 'react-bootstrap';
 
 function Cards() {
 return (
     <>
     <h1 style={{fontFamily:"Acumin,sans-serif;", margin:"30px", padding:"30px"}}>See all our coverage areas</h1>
-    <div className="row">
-        <CardItem
-            title="Zone 1"
-            img={require('../assets/images/NY.jpg').default}
-            subtitle="New York City area"
-        />
-
-        <CardItem
-            title="Zone 2"
-            img={require('../assets/images/HCC.jpg').default}
-            subtitle="Northern New Jersey"
-        />
-    </div>
+    <CardDeck>
+        <Card>
+            <Card.Img className='image-variant' variant="top" src={require('../assets/images/NY9.jpg').default} style={{
+                height:"400px"
+            }} />
+                <Card.Body>
+                    <Card.Title>New York area</Card.Title>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Zone 1</small>
+                </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img className='image-variant' variant="top" src={require('../assets/images/HCC.jpg').default} style={{
+                height:"400px"
+            }}/>
+                <Card.Body>
+                    <Card.Title>Northern New Jersey</Card.Title>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Zone 2</small>
+                </Card.Footer>
+        </Card>
+    </CardDeck>
     </>
 );
 }
